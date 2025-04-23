904053fc484f7b70a7f656f600df392066db60a2
Snehalrajole <snehalrajole123@gmail.com>
2025-04-23 22:59:10 +0530

    Updated BfsDfs.cpp with improved logic

--- BfsDfs.cpp
+++ BfsDfs.cpp
@@ -1,113 +1,108 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <queue>
 using namespace std;
 
+// DFS function using stack
+void dfs(int adj_mat[10][10], int n, int node, int visited[10]) {
+    cout << "DFS Traversal:\n";
 
+    stack<int> stk;
+    stk.push(node);
+    visited[node] = 1;
 
-//adj_mat,n,0,visited
-void dfs(int adj_mat[10][10],int n,int node,int visited[10])
-{
-	cout<<"DFS"<<endl;
+    while (!stk.empty()) {
+        int i = stk.top();
+        stk.pop();
 
-//	return;
-	stack<int> stk;
-	stk.push(node);
-	visited[node] = 1;
+        cout << i << " --> ";
 
-	while(!stk.empty()){
-		int i = stk.top();
-		stk.pop();
-		cout<<i<<"-->";
-		for(int j=n-1;j>=0;j--){
-			if(adj_mat[i][j]==1 && visited[j]!=1){
-				stk.push(j);
-				visited[j] = 1;
-			}
-		}
-	}
-	cout<<"NULL"<<endl;
-
+        for (int j = n - 1; j >= 0; j--) {
+            if (adj_mat[i][j] == 1 && visited[j] != 1) {
+                stk.push(j);
+                visited[j] = 1;
+            }
+        }
+    }
+    cout << "NULL" << endl;
 }
 
-void bfs(int adj_mat[10][10],int n,int node,int visited[10])
-{
-	cout<<"BFS"<<endl;
-//	return;
+// BFS function using queue
+void bfs(int adj_mat[10][10], int n, int node, int visited[10]) {
+    cout << "BFS Traversal:\n";
 
-	queue<int> q;
-	q.push(node);
-	visited[node] = 1;
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
 
-	while(!q.empty()){
-		int i = q.front();
-		q.pop();
+    while (!q.empty()) {
+        int i = q.front();
+        q.pop();
 
-		cout<<i<<"-->";
-		for(int j=0;j<n;j++){
-			if(adj_mat[i][j]==1 && visited[j]!=1){
-				q.push(j);
-				visited[j] = 1;
-			}
-		}
-	}
-	cout<<"NULL"<<endl;
+        cout << i << " --> ";
 
+        for (int j = 0; j < n; j++) {
+            if (adj_mat[i][j] == 1 && visited[j] != 1) {
+                q.push(j);
+                visited[j] = 1;
+            }
+        }
+    }
+    cout << "NULL" << endl;
 }
-
-
-
 
 int main() {
+    int n;
+    int adj_mat[10][10] = {0};
+    int visited[10] = {0};
 
-	int n;
-	int adj_mat[10][10] = {0},visited[10] = {0};
+    cout << "Enter the total number of nodes in the graph --> ";
+    cin >> n;
 
-	cout<<"Enter the total number of nodes in the graph --> ";
-	cin>>n;
+    // Input adjacency matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            char choice;
+            cout << "Do you want to add the edge between " << i << " and " << j << "? (Y/N): ";
+            cin >> choice;
 
+            if (choice == 'Y' || choice == 'y') {
+                adj_mat[i][j] = 1;
+                adj_mat[j][i] = 1;
+            }
+        }
+    }
 
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			cout<<"Do you want to add the Edge between "<<i<<" and "<<j<<"?. (Y or N) ";
-			char s;	cin>>s;
-			if(s=='y' || s=='Y'){
-				adj_mat[i][j] = adj_mat[j][i] = 1;
-			}
-		}
-	}
+    int ch;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. DFS\n";
+        cout << "2. BFS\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> ch;
 
-	int ch;
-	do{
+        switch (ch) {
+            case 1:
+                for (int i = 0; i < n; i++) visited[i] = 0;
+                dfs(adj_mat, n, 0, visited);
+                break;
 
-		cout<<"\n\n";
-		cout<<"1. DFS"<<endl;
-		cout<<"2. BFS"<<endl;
-		cout<<"0. Exit"<<endl;
-		cout<<"Enter Choice:- ";
-		cin>>ch;
-		switch(ch){
-			case 1:
-				for(int i=0;i<n;i++)	visited[i] = 0;
-				cout<<"DFS on the given graph is :- ";
-				dfs(adj_mat,n,0,visited);
-				break;
-			case 2:
-				for(int i=0;i<n;i++)	visited[i] = 0;
-				cout<<"BFS on the given graph is :- ";
-				bfs(adj_mat,n,0,visited);
-				break;
-			case 0:
-				break;
-			default:
-				cout<<"Invalid Choice \n\n";
-			break;
-		}
+            case 2:
+                for (int i = 0; i < n; i++) visited[i] = 0;
+                bfs(adj_mat, n, 0, visited);
+                break;
 
-	}while(ch!=0);
+            case 0:
+                cout << "Exiting the program.\n";
+                break;
 
-	cout<<"Program Finished ";
+            default:
+                cout << "Invalid choice! Please try again.\n";
+                break;
+        }
 
+    } while (ch != 0);
 
-
-
-	return 0;
+    return 0;
 }
